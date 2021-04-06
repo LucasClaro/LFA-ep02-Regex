4bea0a7b882220ae4f6b24ad22d92465ee5e9841
Lucas Claro <lucasbrclaro@gmail.com>
2021-04-06 00:36:09 -0300

    entrada de dados

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ class Extrator
         # @uf = /(?<=[ ,-])[A-Z]{2}(?=[^\w])/
         @tipoLogradouro = /(?:Rua|Av|Av\.|Avenida|R|R\.|Rodovia|Praça|Travessa)/
         @nomeDaRua = /(?<=Rua |Av |Av\. |Avenida |R |R\. |Rodovia |Praça |Travessa )[a-zA-Z0-9!\–º\(\)\-;':\\"\/\. ãáõôêéí]+(?=,)/
-        @numero = /(?<=(Rua |Av |Av\. |Avenida |R |R\. |Rodovia |Praça |Travessa )[a-zA-Z0-9!\–º\(\)\-;':\\"\/\. ãáõôêéí]{1,30})/
+        # @numero = /(?<=(Rua |Av |Av\. |Avenida |R |R\. |Rodovia |Praça |Travessa )[a-zA-Z0-9!\–º\(\)\-;':\\"\/\. ãáõôêéí]{1,30})/
         @geral = /(?:Rua|Av|Av\.|Avenida|R|R\.|Rodovia|Praça|Travessa) [a-zA-Z0-9!\–º\(\)\-;':"\\,\/\. ãáõôêéí]{1,90} (?:[A-Z]{2}|\d{5}-?\d{3})/
         @txt = ""
 
@@ -15,15 +15,38 @@ class Extrator
         @separaCampos = /[a-zA-Z0-9!º\(\);':"\\\/\. ãáõôêéí]{1,}/
         @camposDeDentro = []
 
-      end
-      attr_accessor :cep, :geral, :txt, :uf
+    end
+    attr_accessor :cep, :geral, :txt, :uf
 
     def iniciar
-        file = File.open("exemplo.txt")
+        puts ("Digite 1 para entrar com os dados através de um arquivo")
+        puts ("Digite 2 para entrar com os dados manualmente")
+        num = gets.chomp()
+        if (num == "1")
+            iniciarArquivo
+        else
+            if (num == "2")
+                iniciarGets
+            else
+                puts ('Entrada inválida')
+                iniciar
+            end
+        end
+    end
+
+    def iniciarArquivo
+        puts ("Digite o nome do arquivo txt a ser analisado. (NÃO DIGITE O \".txt\"). O arquivo deve estar junto ao código fonte.")
+        fileName = gets.chomp()
+        file = File.open(fileName+".txt")
         fileData = file.read
         fileData.chomp
         @txt = fileData
         file.close
+    end
+
+    def iniciarGets
+        puts ("Digite o texto a ser analisado.")
+        @txt = gets.chomp()
     end
 
     def lerGeral
@@ -39,26 +62,28 @@ class Extrator
         uf = endereco.scan(@uf).last
         tipoLogradouro = endereco.scan(@tipoLogradouro).first
         nomeDaRua = endereco.scan(@nomeDaRua)
-        numero = endereco.scan(@numero)
-        puts numero
+        # numero = endereco.scan(@numero)
+        # puts numero
         puts
     end
 
     def separaCampos
-        iniciar
-
         coisasDeDentro = []
 
-        for item in @txt
-            coisasDeDentro.append(item.match(dentro)[0])
+        result = @txt.scan(@geral)
+        for item in result
+            coisasDeDentro.append(item.match(@dentro)[0])
         end
 
         for item in coisasDeDentro
-            @camposDeDentro.append(item.scan(separaCampos))
+            @camposDeDentro.append(item.scan(@separaCampos))
         end
+
+        puts @camposDeDentro
     end
 end
+
 e = Extrator.new
 e.iniciar
-e.lerGeral
+# e.lerGeral
 e.separaCampos
